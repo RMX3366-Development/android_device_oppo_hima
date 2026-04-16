641d4fddecda32a23f7d583a56b4375f5fce2ee2
ermyltsor <lamiimuk@163.com>
2026-04-16 14:13:38 +0800

    hima: Explicitly set as an non-AB target
    Change-Id: I3e74c7941ac8b5b7d08635e36622307ccf2edb0b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ include device/oppo/sm8250-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/oppo/hima
 
+# A/B
+AB_OTA_UPDATER := false
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := OP4F0BL1,OP4EA7,hima
 
