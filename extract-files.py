63161c337db64eb227138e0ae60f5c6a2ec7d719
ermyltsor <lamiimuk@163.com>
2026-04-19 11:06:03 +0800

    hima: Update blob list from COS 13.1.0.600(EX01)
    Change-Id: If7905d8dbd31229416ef3ac65fca3ee2245a3939

--- extract-files.py
+++ extract-files.py
@@ -4,18 +4,116 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    File,
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
 
 namespace_imports = [
+    'hardware/oplus',
     'vendor/oppo/sm8250-common',
+    'vendor/qcom/opensource/display',
 ]
+
+def blob_fixup_nop_call(
+    ctx: BlobFixupCtx,
+    file: File,
+    file_path: str,
+    call_instruction: str,
+    disassemble_symbol: str,
+    symbol: str,
+    *args,
+    **kwargs,
+):
+    for line in run_cmd(
+        [
+            llvm_objdump_path,
+            f'--disassemble-symbols={disassemble_symbol}',
+            file_path,
+        ]
+    ).splitlines():
+        line = line.split(maxsplit=3)
+
+        if len(line) != 4:
+            continue
+
+        offset, _, instruction, args = line
+
+        if instruction != call_instruction:
+            continue
+
+        if not args.endswith(f' <{symbol}>'):
+            continue
+
+        with open(file_path, 'rb+') as f:
+            f.seek(int(offset[:-1], 16))
+            f.write(b'\x1f\x20\x03\xd5')  # AArch64 NOP
+
+        break
+
+
+blob_fixups: blob_fixups_user_type = {
+    ('odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libYTCommon.so', 'odm/lib64/libaps_frame_registration.so'): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'odm/lib64/libAlgoProcess.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V6-ndk.so'),
+    'odm/lib64/libOGLManager.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    ('odm/lib/libaiboost_hexagon.so', 'odm/lib64/libaiboost_hexagon.so'): blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open')
+        .clear_symbol_version('remote_handle64_close')
+        .clear_symbol_version('remote_handle64_invoke')
+        .clear_symbol_version('remote_handle64_open'),
+    'odm/lib64/libarcsoft_hta.so': blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open'),
+    'odm/lib64/libsnpe_dsp_domains_v2.so': blob_fixup()
+        .clear_symbol_version('remote_handle64_close')
+        .clear_symbol_version('remote_handle64_invoke')
+        .clear_symbol_version('remote_handle64_open')
+        .clear_symbol_version('remote_register_dma_handle'),
+    'odm/lib64/libarcsoft_portrait_super_night_raw.so': blob_fixup()
+        .clear_symbol_version('remote_handle64_close')
+        .clear_symbol_version('remote_handle64_invoke')
+        .clear_symbol_version('remote_handle64_open')
+        .clear_symbol_version('rpcmem_alloc')
+        .clear_symbol_version('rpcmem_free')
+        .clear_symbol_version('rpcmem_to_fd'),
+    'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
+        .add_needed('libcamera_metadata_shim.so')
+        .binary_regex_replace(b'com.oem.autotest', b'\x00om.oem.autotest'),
+    'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
+        .call(blob_fixup_nop_call, 'bl', '__cfi_check', '_ZN7android8hardware22configureRpcThreadpoolEmb@plt'),
+}  # fmt: skip
 
 module = ExtractUtilsModule(
     'hima',
     'oppo',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
 
